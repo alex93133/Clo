17c19ae43934354077df59851349dbbf68ffe358
Alexandr Lazarev <58854670+alex93133@users.noreply.github.com>
2020-06-18 20:46:18 +0300

    Change app icon

--- Clo/Screens/Add/Fill Info/Controller/TypeSheet.swift
+++ Clo/Screens/Add/Fill Info/Controller/TypeSheet.swift
@@ -1,18 +1,22 @@
 import UIKit
 import FittedSheets
 
-class TypeSheet {
 
+
+class TypeSheet {
+    
+    // MARK: - Properties
     var type = TypeViewController()
     var sheet: SheetViewController!
-
+    
     init(hideCategoryNamedAll: Bool = false) {
         if hideCategoryNamedAll {
             type.clothingTypes.removeFirst()
         }
         setup()
     }
-
+    
+    // MARK: - Functions
     private func setup() {
         let safeZoneHeight: CGFloat        = 70
         let height: CGFloat                = Constants.colorTypeCellHeight * CGFloat(type.clothingTypes.count) + safeZoneHeight
